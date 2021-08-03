6d29bc1a5ae0776e937e764d999298c64160e3e5
Peter Holzer <peter.holzer@agitator.com>
2021-08-03 08:18:17 +0200

    "Create addon: collective.googlecloudlogging"

---
+++ setup.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""Installer for the collective.googlecloudlogging package."""
+
+from setuptools import find_packages, setup
+
+long_description = '\n\n'.join([
+    open('README.rst').read(),
+    open('CONTRIBUTORS.rst').read(),
+    open('CHANGES.rst').read(),
+])
+
+
+setup(
+    name='collective.googlecloudlogging',
+    version='1.0a1',
+    description="Google Cloud Logging Integration",
+    long_description=long_description,
+    # Get more from https://pypi.org/classifiers/
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: Addon",
+        "Framework :: Plone :: 5.2",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+    ],
+    keywords='Python Plone CMS',
+    author='Peter Holzer',
+    author_email='peter.holzer@agitator.com',
+    url='https://github.com/collective/collective.googlecloudlogging',
+    project_urls={
+        'PyPI': 'https://pypi.python.org/pypi/collective.googlecloudlogging',
+        'Source': 'https://github.com/collective/collective.googlecloudlogging',
+        'Tracker': 'https://github.com/collective/collective.googlecloudlogging/issues',
+        # 'Documentation': 'https://collective.googlecloudlogging.readthedocs.io/en/latest/',
+    },
+    license='GPL version 2',
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['collective'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.7",
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+        'z3c.jbot',
+        'plone.api>=1.8.4',
+        'plone.app.dexterity',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            # Plone KGS does not use this version, because it would break
+            # Remove if your package shall be part of coredev.
+            # plone_coredev tests as of 2016-04-01.
+            'plone.testing>=5.0.0',
+            'plone.app.contenttypes',
+            'plone.app.robotframework[debug]',
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    [console_scripts]
+    update_locale = collective.googlecloudlogging.locales.update:update_locale
+    """,
+)
