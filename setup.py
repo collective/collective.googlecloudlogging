38f7813488fb06ac268061fe263960a7296625a8
Peter Holzer <peter.holzer@agitator.com>
2021-08-05 16:53:23 +0200

    cleanup

--- setup.py
+++ setup.py
@@ -49,17 +49,6 @@ setup(
         # -*- Extra requirements: -*-
         "google-cloud-logging"
     ],
-    extras_require={
-        'test': [
-            'plone.app.testing',
-            # Plone KGS does not use this version, because it would break
-            # Remove if your package shall be part of coredev.
-            # plone_coredev tests as of 2016-04-01.
-            'plone.testing>=5.0.0',
-            'plone.app.contenttypes',
-            'plone.app.robotframework[debug]',
-        ],
-    },
     entry_points="""
     [z3c.autoinclude.plugin]
     target = plone
